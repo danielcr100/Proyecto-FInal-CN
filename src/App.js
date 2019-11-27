27615e02bd7aa4ea120c42a59c648b1318e5157f
Daniel Castaneda <danielcr100@gmail.com>
2019-11-26 20:32:48 -0600

    Sofia no hace nada

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import MaterialUi from '@material-ui/core'
+import Logo from './logo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola lista para iniciar la aventura en VINIS con react ???
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Logo />
+
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ src/logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Logo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <img src='./IMG/Logo.jpg' />
+            </div>
+        );
+    }
+}
+
+
+export default Logo;
